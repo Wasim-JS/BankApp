89ba7f27dbe16ae79be927883066d22227af14a5
Wasim Shariff <wasimshariff.02@gmail.com>
2024-05-26 01:07:38 +0530

    second commit

--- bankApp.rb
+++ bankApp.rb
@@ -209,3 +209,103 @@ def main()
 end
 
 main()
+
+
+
+# with class(opps)
+
+class BankAccount
+    #   attr_accessor :username, :password, :balance
+    
+      def initialize(username, password, balance = 5000)
+        @username = username
+        @password = password
+        @balance = balance
+      end
+    
+      def authenticate
+        puts 'Enter the username:'
+        input_username = gets.chomp
+        puts 'Enter the password:'
+        input_password = gets.chomp
+        input_username == @username && input_password == @password
+      end
+    
+      def show_balance
+        if authenticate
+          puts "Your balance: #{@balance}"
+        else
+          puts "Invalid username or password"
+        end
+      end
+    
+      def deposit
+        if authenticate
+          puts 'Enter the amount to deposit:'
+          amount = gets.chomp.to_i
+          @balance += amount
+          puts "Your new balance is #{@balance}"
+        else
+          puts "Invalid username or password"
+        end
+      end
+    
+      def withdraw
+        if authenticate
+          puts 'Enter the amount to withdraw:'
+          amount = gets.chomp.to_i
+          if amount > @balance
+            puts "Insufficient balance"
+          else
+            @balance -= amount
+            puts "Your new balance is #{@balance}"
+          end
+        else
+          puts "Invalid username or password"
+        end
+      end
+    
+      def change_password
+        puts 'Enter the old password:'
+        old_password = gets.chomp
+        if old_password == @password
+          puts 'Enter the new password:'
+          new_password = gets.chomp
+          @password = new_password
+          puts 'Password changed successfully'
+        else
+          puts "Invalid old password"
+        end
+      end
+    
+      def main_menu
+        loop do
+          puts "Enter your choice:\n1. Balance\n2. Deposit\n3. Withdrawal\n4. Change password\n0. Exit"
+          choice = gets.chomp.to_i
+          case choice
+          when 1
+            show_balance
+          when 2
+            deposit
+          when 3
+            withdraw
+          when 4
+            change_password
+          when 0
+            puts "Exiting..."
+            break
+          else
+            puts "Invalid choice"
+          end
+        end
+      end
+    end
+    
+    puts 'Enter the username:'
+    username = gets.chomp
+    puts 'Enter the password:'
+    password = gets.chomp
+    
+    account = BankAccount.new(username, password)
+    account.main_menu
+    
